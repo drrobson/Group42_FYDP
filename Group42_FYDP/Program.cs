2d89cd104bbf260f8c5ac9dc34b3910010c9bf45
Daniel Robson <dan.r.robson@gmail.com>
2011-06-17 20:14:11 -0400

    Committing fundamental solution & project components

---
+++ Group42_FYDP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Research.Kinect.Nui;
+using Microsoft.Research.Kinect.Audio;
+
+namespace Group42_FYDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
