d83a248700f6077a1771334ad28cebb9a20a61dc
unknown <Elliott@.(none)>
2011-06-24 06:00:40 -0400

    added the shared contents file

---
+++ KinAid_Attempt1/SharedContent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+using Microsoft.Research.Kinect.Nui;
+
+namespace KinAid_Attempt1
+{
+    static public class SharedContent
+    {
+        static Dictionary<JointID, Brush> jointColors = new Dictionary<JointID, Brush>() { 
+            {JointID.HipCenter, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.Spine, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.ShoulderCenter, new SolidColorBrush(Color.FromRgb(168, 230, 29))},
+            {JointID.Head, new SolidColorBrush(Color.FromRgb(200, 0,   0))},
+            {JointID.ShoulderLeft, new SolidColorBrush(Color.FromRgb(79,  84,  33))},
+            {JointID.ElbowLeft, new SolidColorBrush(Color.FromRgb(84,  33,  42))},
+            {JointID.WristLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HandLeft, new SolidColorBrush(Color.FromRgb(215,  86, 0))},
+            {JointID.ShoulderRight, new SolidColorBrush(Color.FromRgb(33,  79,  84))},
+            {JointID.ElbowRight, new SolidColorBrush(Color.FromRgb(33,  33,  84))},
+            {JointID.WristRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.HandRight, new SolidColorBrush(Color.FromRgb(37,   69, 243))},
+            {JointID.HipLeft, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.KneeLeft, new SolidColorBrush(Color.FromRgb(69,  33,  84))},
+            {JointID.AnkleLeft, new SolidColorBrush(Color.FromRgb(229, 170, 122))},
+            {JointID.FootLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HipRight, new SolidColorBrush(Color.FromRgb(181, 165, 213))},
+            {JointID.KneeRight, new SolidColorBrush(Color.FromRgb(71, 222,  76))},
+            {JointID.AnkleRight, new SolidColorBrush(Color.FromRgb(245, 228, 156))},
+            {JointID.FootRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))}
+        };
+
+        enum Progression
+        {
+            Completing,
+            Failed,
+            Completed
+        };
+    }
+}
