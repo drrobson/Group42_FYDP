2906803e78d56cb72b24337637605c4087fbc320
unknown <Elliott@.(none)>
2011-07-17 05:49:47 -0400

    started the feedback screen (for real this time)

---
+++ KinAid_Attempt1/ExerciseFeedback.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinAid_Attempt1
+{
+    /// <summary>
+    /// Interaction logic for ExerciseFeedback.xaml
+    /// </summary>
+    public partial class ExerciseFeedback : UserControl
+    {
+        public ExerciseFeedback()
+        {
+            InitializeComponent();
+        }
+    }
+}
